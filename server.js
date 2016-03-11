cdf6fbbd65f8d34802bf06407b38be1149ef1357
elisecode247 <elisecode247@gmail.com>
2016-03-11 21:51:01 +0000

    update server.js logic for errors

--- server.js
+++ server.js
@@ -16,23 +16,30 @@ app.get('/', function(req, res) {
 app.get('/:string', function(req, res) {
   var myRe = /\D/i;
   if (!myRe.test(req.params.string)) {
+    console.log("unix format")
     var timestamp = Date.utc.create(parseInt(req.params.string));
     if (Date.create(timestamp).format('{Month} {d}, {yyyy}') === "Invalid Date") {
       res.send(timeobject)
+      console.log("not valid unix format")
     }
     else {
       timeobject.unix = req.params.string;
       timeobject.natural = Date.create(timestamp).format('{Month} {d}, {yyyy}');
       res.send(timeobject)
+      console.log("valid unix format")
     }
   }
   else if (new Date(req.params.string).isValid() === true) {
     var naturalDate = Date.create(req.params.string);
+    console.log("valid natural format")
     timeobject.unix = Date.parse(naturalDate);
     timeobject.natural = Date.create(naturalDate).format('{Month} {d}, {yyyy}');
     res.send(timeobject)
   }
   else {
+    console.log("error")
+    timeobject.unix = null;
+    timeobject.natural = null;
     res.send(timeobject)
   }
 });
